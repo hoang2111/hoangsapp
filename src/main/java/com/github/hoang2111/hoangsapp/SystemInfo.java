2aa343feaa47fe38eafcbd2471ea5c640fdbde3f
macbook <macbook@localhost>
2024-07-12 17:43:21 +0700

    Commit message goes here

---
+++ src/main/java/com/github/hoang2111/hoangsapp/SystemInfo.java
@@ -0,0 +1,13 @@
+package com.github.hoang2111.hoangsapp;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.github.hoang2111.hoangsapp {
+    requires javafx.controls;
+    exports com.github.hoang2111.hoangsapp;
+}
